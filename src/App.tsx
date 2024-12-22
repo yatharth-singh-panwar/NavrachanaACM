c8c6ebc62585a5ca925e8d2f42243f52d424b071
Dharmin Nagar <dharminnagar@gmail.com>
2024-12-22 13:39:24 +0530

    Fixed /Events

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import "./App.css";
 import Events from "../src/pages/Events";
 import { Landing } from "./pages/Landing";
 import { Team } from "./pages/Team";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Admin/Login";
 import Dashboard from "./pages/Admin/Dashboard";
+
 function App() {
   return (
     <>
@@ -13,8 +14,9 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="Team" element={<Team />} />
           <Route path="Events" element={<Events />} />
-          <Route path="admin/login" element={<Login/>}/>
-          <Route path="admin/dashboard" element={<Dashboard/>}/>
+          <Route path="admin/login" element={<Login />} />
+          <Route path="admin/dashboard" element={<Dashboard />} />
+          <Route path="admin" element={<Navigate to="login" />} />
         </Routes>
       </BrowserRouter>
     </>
